b8dfb98052c6dd0ca1fe082c39895f7d6df75f5b
somewhat9 <179911515+somewhat9@users.noreply.github.com>
2025-07-22 02:11:30 -0400

    Structured Game Model
    Added fields to the Game struct to structure the model of the game.

--- internal/game/game.go
+++ internal/game/game.go
@@ -2,6 +2,22 @@ package game
 
 import "github.com/somewhat9/pong-go/internal/config"
 
-type Game struct{
+type Game struct {
 	Cfg *config.Config
+	p1 paddle
+	p2 paddle
+	ball
+}
+
+type paddle struct {
+	score int
+	y float32
+	dy float32
+}
+
+type ball struct {
+	x float32
+	y float32
+	dx float32
+	dy float32
 }
